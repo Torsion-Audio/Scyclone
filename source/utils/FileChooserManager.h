f137ba55778c34c6193d930998a10ff1402c665b
timbencker <tim.bencker@gmx.de>
2026-06-13 14:43:54 +0200

    fix(filechooser): repair FileChooserManager header and lifetime members

--- source/utils/FileChooserManager.h
+++ source/utils/FileChooserManager.h
@@ -1,20 +1,19 @@
 #pragma once
 #include <JuceHeader.h>
-#include "FileChooserManager.h"
 #include "PluginProcessor.h"
+#include "WarningWindow.h"
 
 class FileChooserManager
 {
 public:
     FileChooserManager(AudioPluginAudioProcessor& processor);
+    ~FileChooserManager();
 
     // Generalized file chooser function, atm for single files only
     void openFileChooser(const juce::String& dialogTitle,
                          const juce::File& initialDirectory,
                          const juce::String& filePatterns,
                          std::function<void(const juce::File&)> onValidFileChosenCallback);
-
-    bool fileHasValidExtension(const juce::File& file, const juce::String& filePatterns);
 
     // Network-specific file chooser function, using openFileChooser
     void openFileChooserForNetwork(int networkID);
@@ -27,4 +26,7 @@ private:
     juce::File lastOpenedFolder;
 
     WarningWindow warningWindow;
+
+    std::shared_ptr<std::atomic<bool>> callbackAlive;
+    std::shared_ptr<std::atomic<bool>> chooserOpen;
 };
