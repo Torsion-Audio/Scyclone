ab2e537abf3256d3e8892d8f6da95b4c6213a404
michaelwitte <michael-witte@hotmail.de>
2026-06-13 15:03:06 +0200

    Fix(cmake): pin minimum version for libsamplerate integration

--- cmake/setup_libsamplerate.cmake
+++ cmake/setup_libsamplerate.cmake
@@ -1,5 +1,7 @@
 set(LIBSAMPLERATE_TESTS OFF CACHE BOOL "Disable deprecated LIBSAMPLERATE_TESTS option" FORCE)
 
+set(CMAKE_POLICY_VERSION_MINIMUM 3.5)
+
 add_subdirectory(${CMAKE_CURRENT_SOURCE_DIR}/modules/libsamplerate)
 
 target_link_libraries(${TARGET_NAME} PRIVATE samplerate)
